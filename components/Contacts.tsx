82ad4ad092fe3da6889d191c38783d53b4e65f88
fjt-dev <157100279+fjt-dev@users.noreply.github.com>
2026-06-18 09:21:49 +0900

    fix: typo

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -1,13 +1,13 @@
 export default function Contacts() {
   return (
     <div className="flex gap-4 text-[#007bff] underline">
-      <a href="https://github.com/fjtd-dev" target="_blank" className="cursor-pointer">
+      <a href="https://github.com/fjt-dev" target="_blank" className="cursor-pointer">
         GitHub
       </a>
       <a href="https://x.com/@fjtd_dev" target="_blank" className="cursor-pointer">
         X(Twitter)
       </a>
-      <a href="https://primisskey.space/@fjtd" target="_blank" className="cursor-pointer">
+      <a href="https://prismisskey.space/@fjtd" target="_blank" className="cursor-pointer">
         Missky
       </a>
     </div>
