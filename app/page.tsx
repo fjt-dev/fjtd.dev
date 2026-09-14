3acadb6a05cb2bf5dcae9f9bc2782c9a3c3acafd
fjt-dev <157100279+fjt-dev@users.noreply.github.com>
2026-09-14 19:49:13 +0900

    chore: upgrade dependencies and add Cache Components opt-out

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import Contacts from '@/components/Contacts';
 import Footer from '@/components/Footer';
 import ContentsCard from '@/components/ContentsCard';
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto py-12 px-4">
